46cdf8e107bc09b3311515e3b49a382b576a9fb1
JrMagril <jr.magril@hotmail.com>
2026-03-17 06:04:17 -0300

    estudo

--- exercicios/1-CRUD/2-alterar-tabelas.sql
+++ exercicios/1-CRUD/2-alterar-tabelas.sql
@@ -1,3 +1,14 @@
 -- Na tabela salario_classe altere o atributo "nivel" para que seu tipo de dado seja varchar, com tamanho máximo de 50 caracteres
+CREATE TABLE salario_classe_dois (
+  id_salario INTEGER PRIMARY KEY AUTOINCREMENT,
+  nivel varchar(50),
+  salario INTEGER
+);
+
+DROP TABLE salario_classe;
+
+ALTER TABLE salario_classe_dois RENAME TO salario_classe;
 
 -- Na tabela employees, adicione uma coluna para receber o id_salario
+ALTER TABLE employees
+ADD id_salario integer;
--- exercicios/1-CRUD/3-inserir-dados.sql
+++ exercicios/1-CRUD/3-inserir-dados.sql
@@ -1,4 +1,17 @@
 -- Insira 3 classes salariais na tabela salario_classe
+INSERT INTO salario_classe
+VALUES
+(1, 3500, 'Estagiário');
+
+
+INSERT INTO salario_classe
+VALUES
+(2, 4700, 'Iniciante I'),
+(3, 5300, 'Iniciante II');
 
 
 -- Insira 1 nova classe salarial, sem declarar o atributo id_salario
+INSERT INTO salario_classe (nivel, salario)
+VALUES
+(8500, 'Gerente'),
+(10700, 'Coordenador');
